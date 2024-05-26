36db6add3db87d27a08fb6e05cd62a13ed30cbdc
Mohammed-Mohad <mohamed.mehad@aastustudent.edu.et>
2024-05-26 18:45:10 +0300

    done

--- app.js
+++ app.js
@@ -1,50 +1,71 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
-const getMessage = require("./notes.js")
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
 
 // create add command
 
 yargs.command({
-    command: 'add',
-    describe: 'add a new note',
-    builder:{
-        title:{
-            describe:'note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body:{
-            describe: "Body option",
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  describe: "add a new note",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
     },
-    handler: function(argv){
-        console.log("Title: ", yargs.argv.title);
-        console.log("Body: ", argv.body);
-    }
-})
+    body: {
+      describe: "Body option",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
 
 // create a remove command
 
 yargs.command({
-    command: 'remove',
-    describe: 'remove note',
-    handler: function(){
-        console.log("Removed a note");
-    }
-})
+  command: "remove",
+  describe: "remove note",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
+  },
+});
+
+// list command
+
+yargs.command({
+  command: "list",
+  describe: "list note",
+  handler: (argv) => {
+    notes.listNote();
+  },
+});
 
 // create a Read command
 
 yargs.command({
-    command: 'read',
-    describe: 'read a note',
-    handler: function(){
-        console.log("reading...");
+  command: "read",
+  describe: "read a note",
+  builder:{
+    title:{
+    describe: "note title",
+    demandOption: true,
+    type: 'string'
     }
-})
+  },
+  handler: function (argv) {
+   notes.readNote(argv.title)
+  },
+});
 
-yargs.parse()
-
-console.log("Hello world!");
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,5 +1,81 @@
-let getMessage = (message)=>{
-    return message
-}
+const fs = require("fs");
+const chalk = require("chalk");
+const { log } = require("console");
 
-module.exports = getMessage
+const addNote = function (title, body) {
+  const notes = loadNote();
+  const duplicateNote = notes.filter(function (note) {
+    return note.title === title;
+  });
+  console.log("Duplicate note:", duplicateNote);
+  if (duplicateNote.length !== 0) {
+    console.log(chalk.bgRed("Note title is Taken"));
+  } else {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNote(notes);
+    console.log(chalk.bgGreen("Note Added!"));
+  }
+};
+//save Note
+
+const saveNote = function (notes) {
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync("note.json", dataJson);
+};
+
+//load Note
+const loadNote = function () {
+  try {
+    const dataBuffer = fs.readFileSync("note.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+//remove note
+
+const removeNote = function (title) {
+  const notes = loadNote();
+  const removeNote = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (notes.length !== removeNote.length) {
+    saveNote(removeNote);
+    console.log(chalk.bgGreen("Note removed"));
+  } else {
+    console.log(chalk.bgRed("No note founded"));
+  }
+  console.log("removeNote: ", removeNote);
+};
+
+//List Note title
+
+const listNote = () => {
+  const notes = loadNote();
+  console.log(chalk.bgGreen("Your notes"));
+  notes.forEach((note) => console.log(note.title));
+};
+// read note
+
+const readNote = (title) => {
+  const notes = loadNote();
+  const noteToRead = notes.find((note) => note.title === title);
+  if (noteToRead) {
+    console.log(chalk.inverse(noteToRead.title));
+    console.log(noteToRead.body);
+  }
+  else console.log(chalk.bgRed("title not exist"));
+};
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote,
+  listNote: listNote,
+  readNote: readNote,
+};
